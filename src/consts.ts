99599029cb4953956e9940c1747b450bb4b31935
anhtuanm <anhtuan.mai19@gmail.com>
2026-05-07 10:57:41 +0200

    Update some content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Lotus Meditation";
+export const SITE_DESCRIPTION =
+	"A non-profit club promoting well-being and personal growth through meditation. 100% free, community-driven, open to everyone.";
