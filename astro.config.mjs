0118d1c73904cee69252369895c6661a3bef6c31
anhtuanm <anhtuan.mai19@gmail.com>
2026-05-07 09:59:38 +0200

    Add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lotusmeditation.github.io",
+	site: "https://lotusmeditation.org",
 	integrations: [mdx(), sitemap()],
 });
